df479f3a08da9442b7edfa3e8d04a98ad6a2e075
shiyc <linkinshi@gmail.com>
2018-07-17 13:12:15 +0800

    修复拼接查询参数时会多加一个&符号和多移除最后一位的bug

--- Apollo/Util/QueryUtils.cs
+++ Apollo/Util/QueryUtils.cs
@@ -11,6 +11,10 @@ namespace Com.Ctrip.Framework.Apollo.Util
     {
         public static string Build(IReadOnlyCollection<KeyValuePair<string, string>> source)
         {
+            if (source == null || source.Count == 0)
+            {
+                return "";
+            }
             var sb = new StringBuilder(source.Count * 32);
 
             foreach (var kv in source)
@@ -21,7 +25,7 @@ namespace Com.Ctrip.Framework.Apollo.Util
                 sb.Append(WebUtility.UrlEncode(kv.Value));
             }
 
-            return sb.ToString(0, sb.Length - 1);
+            return sb.ToString(1, sb.Length - 1);
         }
     }
 }
