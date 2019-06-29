c786ac39621f021a804004543cfe8cae901c9853
彭伟 <pengwei@tuhu.cn>
2019-06-29 09:55:47 +0800

    更新文档说明

--- Apollo.AspNetCore.Demo/Program.cs
+++ Apollo.AspNetCore.Demo/Program.cs
@@ -18,8 +18,10 @@ namespace Apollo.AspNetCore.Demo
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-               .ConfigureAppConfiguration((cotnext, builder) => builder
-                    .AddApollo(cotnext.Configuration.GetSection("apollo"))
+               //.ConfigureAppConfiguration((context, builder) => builder //使用环境变量、命令行之类，建议Docker中运行使用此方式
+               //     .AddApollo(context.Configuration.GetSection("apollo"))
+               .ConfigureAppConfiguration(builder => builder //普通方式，一般配置在appsettings.json中
+                   .AddApollo(builder.Build().GetSection("apollo"))
                     .AddDefault(ConfigFileFormat.Xml)
                     .AddDefault(ConfigFileFormat.Json)
                     .AddDefault(ConfigFileFormat.Yml)
