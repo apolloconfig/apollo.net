06bbc72df87d89b616846ddfcf8344e058235d08
彭伟 <pengwei@tuhu.cn>
2019-10-18 09:50:38 +0800

    nullable

--- Apollo/ConfigExtensions.cs
+++ Apollo/ConfigExtensions.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
-#nullable enable
 namespace Com.Ctrip.Framework.Apollo
 {
     public static partial class ConfigExtensions
