40113de5cbc57ac95b6e4813e509ed7749e0feb8
pengweiqhca <pengweiqhca@sina.com>
2023-08-08 16:16:35 +0800

    Split IConfigurationSource from ApolloConfigurationProvider

---
+++ src/Apollo.Configuration/ApolloConfigurationSource.cs
@@ -0,0 +1,28 @@
+﻿using Com.Ctrip.Framework.Apollo.Internals;
+
+namespace Com.Ctrip.Framework.Apollo;
+
+internal class ApolloConfigurationSource : IConfigurationSource
+{
+    private readonly string? _sectionKey;
+    private readonly IConfigRepository _configRepository;
+    private Task? _initializeTask;
+
+    public ApolloConfigurationSource(string? sectionKey, IConfigRepository configRepository)
+    {
+        _sectionKey = sectionKey;
+        _configRepository = configRepository;
+        _initializeTask = configRepository.Initialize();
+    }
+
+    public IConfigurationProvider Build(IConfigurationBuilder builder)
+    {
+        Interlocked.Exchange(ref _initializeTask, null)?.ConfigureAwait(false).GetAwaiter().GetResult();
+
+        return new ApolloConfigurationProvider(_sectionKey, _configRepository);
+    }
+
+    public override string ToString() => string.IsNullOrEmpty(_sectionKey)
+        ? $"apollo {_configRepository}"
+        : $"apollo {_configRepository}[{_sectionKey}]";
+}
